873217f7fcb5e3c487103dc7cfb6ae7ac1b55d38
M-AzizSrairi <azizsrairi00@gmail.com>
2024-01-15 20:17:24 +0100

    setting up tailwind css

---
+++ api-interface/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'blue': '#082f49',    /* Royal Blue */
+      'emerald': '#10b981',        /* Emerald Green*/
+      'orange': '#d97706',       /* Orange-Red */
+      'neutral': '#d4d4d4',      /* Neutral */
+      'gray' : '#111827',
+      'black' : '#0c0a09',
+    },
+    backgroundImage : {
+           'parallax' : 'url("./Assets/team.jpg")',
+           'parallax2' : 'url("./Assets/pitch.jpg")',
+    }
+  },
+  plugins: [],
+};
